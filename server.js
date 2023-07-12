20669df3bd84defe7434b29f3040609664936dc4
Ak Techy <107804394+missnora07@users.noreply.github.com>
2023-07-12 18:10:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,99 @@
 const express = require('express');
 const app = express();
-const ytdl = require('ytdl-core');
-const cors = require('cors');
+const { exec } = require('child_process');
+const ffmpeg = require('fluent-ffmpeg');
+comst cors = require('cors');
 
+// Endpoint to handle YouTube video download
 app.use(cors());
-app.get('/youtube', async (req, res) => {
-  const videoUrl = req.query.url;
+app.get('/download', (req, res) => {
+  const { quality, url, videoOrAudio } = req.query; // Parameters passed in JSON format
 
-  if (!ytdl.validateURL(videoUrl)) {
-    res.status(400).json({ error: 'Invalid YouTube URL' });
-    return;
+  // Validate the parameters
+  if (!quality || !url || !videoOrAudio) {
+    return res.status(400).json({ error: 'Missing parameters' });
   }
 
-  try {
-    const info = await ytdl.getInfo(videoUrl);
-    const { title, thumbnails } = info.videoDetails;
-    const thumbnail = thumbnails[0]?.url || '';
+  // Download options
+  let format = `bestvideo[height<=${quality}]+bestaudio/best[height<=${quality}]`;
+  let onlyAudio = false;
 
-    const formats = info.formats.filter(format => format.audioChannels === 2);
-    const formattedFormats = formats.map((format, index) => ({
-      itag: format.itag,
-      quality: format.qualityLabel || format.quality,
-      mimeType: format.mimeType,
-      url: format.url,
-      info: 'With sound',
-    }));
-    res.json({ title, thumbnail, files:formattedFormats });
-  } catch (error) {
-    console.error('Error occurred:', error);
-    res.status(500).json({ error: 'An error occurred' });
+  if (videoOrAudio === 'audio') {
+    format = 'bestaudio/best';
+    onlyAudio = true;
   }
+
+  // Create a unique output filename for the downloaded file
+  const outputFilename = `output.${onlyAudio ? 'mp3' : 'mp4'}`;
+
+  // Function to download a video or audio file using youtube-dl
+  function downloadFile(url, outputFilename, format, onlyAudio) {
+    const command = `youtube-dl -f ${format} -o ${outputFilename} ${url}`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).json({ error: 'An error occurred during the download' });
+      }
+
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return res.status(500).json({ error: 'An error occurred during the download' });
+      }
+
+      console.log(`Downloaded ${outputFilename}`);
+
+      if (onlyAudio) {
+        // If only audio is requested, convert the downloaded file to an audio-only format
+        const outputPath = `./downloads/${outputFilename.replace(/\.[^/.]+$/, '.mp3')}`;
+
+        ffmpeg(outputFilename)
+          .noVideo()
+          .audioCodec('libmp3lame')
+          .output(outputPath)
+          .on('end', () => {
+            console.log(`Converted to audio: ${outputPath}`);
+            // Delete the original downloaded file after conversion
+            fs.unlink(outputFilename, (err) => {
+              if (err) {
+                console.error('Error deleting file:', err);
+              }
+            });
+            // Send the converted audio file to the client
+            res.download(outputPath, outputFilename, () => {
+              // Delete the converted audio file after it has been sent to the client
+              fs.unlink(outputPath, (err) => {
+                if (err) {
+                  console.error('Error deleting file:', err);
+                }
+              });
+            });
+          })
+          .on('error', (err) => {
+            console.error('Conversion error:', err);
+            return res.status(500).json({ error: 'An error occurred during the conversion' });
+          })
+          .run();
+      } else {
+        // Send the downloaded video file to the client
+        res.download(outputFilename, outputFilename, () => {
+          // Delete the downloaded video file after it has been sent to the client
+          fs.unlink(outputFilename, (err) => {
+            if (err) {
+              console.error('Error deleting file:', err);
+            }
+          });
+        });
+      }
+    });
+  }
+
+  // Call the downloadFile function with the provided options
+  downloadFile(url, outputFilename, format, onlyAudio);
 });
 
-const port = 3000;
+// Start the server
+const port = 3000; // Choose a port for your server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
