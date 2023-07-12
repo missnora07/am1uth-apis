2f227a97f6a94322e5533e6509411579b5fc1d58
Ak Techy <107804394+missnora07@users.noreply.github.com>
2023-07-12 17:45:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,17 +17,15 @@ app.get('/youtube', async (req, res) => {
     const { title, thumbnails } = info.videoDetails;
     const thumbnail = thumbnails[0]?.url || '';
 
-    const files = info.formats.map((format) => {
-      const withSound = format.hasAudio ? '' : 'No sound';
-      return {
-        itag: format.itag,
-        quality: format.qualityLabel || format.quality,
-        mimeType: format.mimeType,
-        url: format.url,
-        info: withSound,
-      };
-    });
-    res.json({ title, thumbnail, files });
+    const formats = info.formats.filter(format => format.audioChannels === 2);
+    const formattedFormats = formats.map((format, index) => ({
+      itag: format.itag,
+      quality: format.qualityLabel || format.quality,
+      mimeType: format.mimeType,
+      url: format.url,
+      info: 'With sound',
+    }));
+    res.json({ title, thumbnail, files:formattedFormats });
   } catch (error) {
     console.error('Error occurred:', error);
     res.status(500).json({ error: 'An error occurred' });
